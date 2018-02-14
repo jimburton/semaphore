04a665a21ffd47403637fdf69dec347fbec11089
Jim Burton <j.burton@brighton.ac.uk>
2018-02-14 18:30:17 +0000

    Code working

---
+++ src/CI346/Main.java
@@ -0,0 +1,22 @@
+package CI346;
+
+import CI346.semaphore.Librarian;
+import CI346.semaphore.Student;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.logging.Logger;
+
+public class Main {
+
+    public static Logger LOG = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
+        Librarian lib = new Librarian(3);
+        int numStudents = 10;
+
+        for(int i=0; i<numStudents;i++) {
+            int work = ThreadLocalRandom.current().nextInt(3000, 10000);
+            new Student(lib, work).start();
+        }
+    }
+}
