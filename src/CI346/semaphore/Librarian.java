b9dc3acd725af938318959c5f212a84beb745663
Jim Burton <j.burton@brighton.ac.uk>
2018-02-14 19:03:42 +0000

    Moved solution

--- src/CI346/semaphore/Librarian.java
+++ src/CI346/semaphore/Librarian.java
@@ -2,22 +2,4 @@ package CI346.semaphore;
 
 public class Librarian {
 
-    private final int numDesks;
-    private int studentsWorking = 0;
-
-    public Librarian(int numDesks) {
-        this.numDesks = numDesks;
-    }
-
-    public synchronized void requestDesk() throws InterruptedException {
-        while(numDesks == studentsWorking) wait();
-        studentsWorking++;
-        notify();
-    }
-
-    public synchronized void releaseDesk() throws InterruptedException {
-        while(studentsWorking == 0) wait();
-        studentsWorking--;
-        notify();
-    }
 }
--- src/CI346/semaphore/Student.java
+++ src/CI346/semaphore/Student.java
@@ -1,28 +1,5 @@
 package CI346.semaphore;
 
-import CI346.Main;
-
 public class Student extends Thread {
 
-    private final Librarian librarian;
-    private final long work;
-
-    public Student(Librarian librarian, long work) {
-        this.librarian = librarian;
-        this.work = work;
-    }
-
-    @Override
-    public void run() {
-        try {
-            Main.LOG.info(String.format("Student %s would like to work", getName()));
-            librarian.requestDesk();
-            Main.LOG.info(String.format("Student %s WORKING", getName()));
-            sleep(work);
-            librarian.releaseDesk();
-            Main.LOG.info(String.format("Student %s FINISHED", getName()));
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
 }
